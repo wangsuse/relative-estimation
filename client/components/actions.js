e410e233fece462e88189360a6945081f294c039
wangsuse <wangsuse2011@gmail.com>
2020-05-26 23:53:41 -0400

    support upload

--- client/components/actions.js
+++ client/components/actions.js
@@ -55,6 +55,19 @@ const ExportButton = styled.button`
   margin: 0 2px 0 2px;
 `;
 
+const ImportButton = styled.button`
+  background-color: cornflowerblue;
+  border: none;
+  color: white;
+  padding: 5px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 13px;
+  margin: 0 2px 0 2px;
+  width: 300px;
+`;
+
 
 const UrlContainer = styled.div`
   padding: 6px;
@@ -100,12 +113,26 @@ class Actions extends React.Component {
     this.props.dispatchUpdatePlanner(newPlanner)
   }
 
+  handleImport(event){
+    const reader = new FileReader();
+    reader.onload = this.onReaderLoad.bind(this);
+    reader.readAsText(event.target.files[0]);
+  }
+
+  onReaderLoad(event){
+    console.log(event.target.result);
+    const planner = JSON.parse(event.target.result);
+    this.props.dispatchUpdatePlanner(planner);
+  }
+
+
+
   render() {
     return (
       <Container>
         <AddCards></AddCards>
         <Button onClick={this.handleClearBoard.bind(this)}>Reset Board(Clear all)</Button>
-        <ExportButton onClick={this.handleExport.bind(this)}>Export/Download</ExportButton>
+
         <Droppable droppableId="deleteZone">
           {(provided, snapshot) => (
             <DeleteZone
@@ -127,6 +154,8 @@ class Actions extends React.Component {
             onFocusOut={this.handleClickJiraUrl.bind(this)}
           />
         </UrlContainer>
+        <ExportButton onClick={this.handleExport.bind(this)}>Export/Download</ExportButton>
+        <ImportButton ><input id="file" type="file" onChange={this.handleImport.bind(this)}/>Import</ImportButton>
 
       </Container>
     );
